3401ce33ec3d5d50a05e4f7b40c3085ac31819c1
Salimchisti <muhammedsalim26102018@gmail.com>
2024-11-15 10:13:07 +0530

    slight changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import HeroPage from '../Hero/HeroPage';
 import Excellence from './Excellence';
 import Doctors from '../pages/Doctors'; // Import the Doctors component
 import DirectorPage from './DirectorPage'
+import Specialities from './Specialities';
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <Hero />
       <Excellence />  
       <HeroPage />
+      <Specialities />
       <Doctors />  
       <DirectorPage />
     </Layout>
